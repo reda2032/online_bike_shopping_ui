4f08c07433bc7bc41828ae6b10d706c2c5f18383
reda2032 <reda.ahmed.2032@gmail.com>
2025-04-21 20:10:32 +0200

    add assets

--- lib/core/style/images/app_images.dart
+++ lib/core/style/images/app_images.dart
@@ -1,3 +1,24 @@
 class AppImages {
   static const String pageUnderBuild = 'assets/images/core/page_under_build.jpg';
+  static const String bike01 = 'assets/images/home/bike01.png';
+  static const String bike02 = 'assets/images/home/bike02.png';
+  static const String bike03 = 'assets/images/home/bike03.png';
+  static const String helmet = 'assets/images/home/helmet.png';
+
+  //  SVG icons
+  static const String accessory_icon = 'assets/images/svg/accessory_icon.svg';
+  static const String bicycle_icon = 'assets/images/svg/bicycle_icon.svg';
+  static const String bike_icon = 'assets/images/svg/bike_icon.svg';
+  static const String cart_fill_icon = 'assets/images/svg/cart_fill_icon.svg';
+  static const String search_icon = 'assets/images/svg/search_icon.svg';
+  static const String chevron_forward_ico = 'assets/images/svg/chevron_forward_icon.svg';
+  static const String chevron_left_icon = 'assets/images/svg/chevron_left_icon.svg';
+  static const String doc_text_fill_icon = 'assets/images/svg/doc_text_fill_icon.svg';
+  static const String heart_icon = 'assets/images/svg/heart_icon.svg';
+  static const String map_fill_icon = 'assets/images/svg/map_fill_icon.svg';
+  static const String minus_icon = 'assets/images/svg/minus_icon.svg';
+  static const String mountain_icon = 'assets/images/svg/mountain_icon.svg';
+  static const String person_fill_icon = 'assets/images/svg/person_fill_icon.svg';
+  static const String plus_icon = 'assets/images/svg/plus_icon.svg';
+  static const String road_icon = 'assets/images/svg/road_icon.svg';
 }
