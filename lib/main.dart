fd0686f243b452bbde0f01473791951eaa09a575
reda2032 <reda.ahmed.2032@gmail.com>
2025-04-21 12:10:45 +0200

    Device Orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:online_bike_shopping/online_bike_shopping_app.dart';
 
-void main() {
-  runApp(const OnlineBikeShoppingApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations(
+    [DeviceOrientation.portraitDown, DeviceOrientation.portraitUp],
+  ).then((_) {
+    runApp(const OnlineBikeShoppingApp());
+  });
 }
